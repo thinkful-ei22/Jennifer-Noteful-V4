3dc86de0a592609605aa7d5f9014c08d26f60668
Jennifer Colna <jennifer.colna@gmail.com>
2018-07-17 10:36:29 -0500

    token received on login

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,25 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET, JWT_EXPIRY} = require('../config');
+
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
 
-router.post('/login', localAuth, function (req, res) {
-  return res.json(req.user);
+const createAuthToken = function(user){
+  return jwt.sign({user}, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY,
+    algorithm: 'HS256'
+  });
+};
+
+router.post('/login', localAuth,(req, res) => {
+  const authToken = createAuthToken(req.user);
+  return res.json({authToken});
 });
 
 module.exports = router;
